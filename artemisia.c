cc4497462f6d1786d6260a3ca6c4c26f15bb58f6
Peter Allan <alwynallan@gmail.com>
2022-06-15 11:11:21 -0400

    Update artemisia.c
    MIT License

--- artemisia.c
+++ artemisia.c
@@ -1,12 +1,25 @@
 // artemisia.c
 
-// detects defects in f_cker, old_rand
+// Copyright 2022 alwynallan@gmail.com, MIT License
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+// detects defects in f_cker, old_rand, most compressed files
+
+// $ sudo apt install libgsl-dev
+// $ gcc -Wall -O3 -o artemisia artemisia.c -l gsl
+// $ ln -s artemisia artemisia8
+// $ ln -s artemisia artemisia16
+// $ ln -s artemisia artemisia24
+// $ ln -s artemisia artemisia32
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <libgen.h> // basename()
-#include <math.h>
+#include <math.h> // M_E
 #include <assert.h>
 #include <gsl/gsl_cdf.h> // $ sudo apt install libgsl-dev
 
